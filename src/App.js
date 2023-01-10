d1715d8e935c2fa92b4330254e85f80a4fa7dff3
andriivam <andriivlasiuk2@gmail.com>
2023-01-10 10:52:22 +0100

    create nav bar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import Footer from './Components /HomePage/Footer';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        <Routes>
+        </Routes>
+        <Footer/>
+      </Router>
+ 
     </div>
   );
 }
