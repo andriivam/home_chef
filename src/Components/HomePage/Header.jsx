2326ae75356d15211f0a5bc37721dad6f2c5f4e8
Kyubs1610 <diego.decort@gmail.com>
2023-01-10 14:23:49 +0100

    last commit nav bar

--- src/Components/HomePage/Header.jsx
+++ src/Components/HomePage/Header.jsx
@@ -7,24 +7,25 @@ import { FaSearch } from 'react-icons/fa';
 const Header = () => {
     const inputRef = useRef();
     return( 
-        <header className="App-header">
+        <header>
+        <div className="App-header">
             <div className='smlLogo'>
                 <img className ='smallLogo' src={image} alt="Brand Logo"></img>
             </div>
-            
-			<div action="" className="search-bar">
+            <div action="" className="search-bar">
             <input
                 type="text"
                 placeholder=" Search here"
                 className="search-input"
                 ref={inputRef} /> 
-
                 <button className='search-btn' type="submit"><FaSearch /></button>
-                 
             </div>
-            <hr className="line"></hr> 
-            
+            </div>
+            <div>
+                <hr/>
+            </div>
         </header>
+        
     )
 }
 export default Header;
