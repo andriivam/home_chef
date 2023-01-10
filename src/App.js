ead1661393d54c4dda97303012eddfc85092e280
andriivam <andriivlasiuk2@gmail.com>
2023-01-10 14:46:47 +0100

    create chef list comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Footer from './Components /HomePage/Footer';
+import ChefList from './Components /Menu/ChefList';
 import {BrowserRouter as Router, Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      <ChefList/>
       <Router>
         <Routes>
         </Routes>
