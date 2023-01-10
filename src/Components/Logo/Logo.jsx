c6b39094926e37a5be95cc87d55264515bd32324
Kawtar <kawtarzarioh@gmail.com>
2023-01-10 16:31:56 +0100

    add logo page

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,1 +1,35 @@
+import React from "react";
+// import { useMediaQuery } from 'react-responsive'
+import "./Logo.css";
+import image from "./images/image.png";
+// import { Button } from 'react-bootstrap';
+// import { useHistory } from "react-router-dom";
 
+export default function Logo() {
+  return (
+    <div className="logo">
+      <button className="logoButton">
+        <img src={image} />
+      </button>
+    </div>
+  );
+}
+
+// class redirection extends Component {
+
+//     faireRedirection=()=> {
+//       let url = "maNouvelleURL"
+//       let history = useHistory()
+//       history.push(url)
+//     }
+
+//     render() {
+//       return (
+//         <div>
+//         <Button onClick={this.faireRedirection} style={{backgroundImage:"url('./images/images.png')",backgroundSize:"cover", width:"40px", height:"40px"}}>
+
+//         </Button>
+//         </div>
+//       )
+//     }
+//   }
