c50932c140d06a2fed2b80fc2627b6bf3d108aac
Kyubs1610 <diego.decort@gmail.com>
2023-01-12 12:01:36 +0100

    commit for the UserForm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from './Components/HomePage/Footer';
 import {BrowserRouter as Router, Routes} from 'react-router-dom';
 import Header from './Components/HomePage/Header'
 import MainSection from './Components/HomePage/MainSection';
+import UserForm from './Components/UserForm/UserForm';
+
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,9 @@ function App() {
       <Router>
         <Routes>
         </Routes>
+        <Footer/>
+        <Header />
+        <UserForm />
         <Footer/>
       </Router>
  
