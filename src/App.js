b6406153f61b7bdee3b2290d3c3f419fdd0e2eec
prati <pratima.sampath@gmail.com>
2023-01-23 12:51:46 +0100

    connecting db to frontend

--- src/App.js
+++ src/App.js
@@ -2,43 +2,44 @@ import React from 'react'
 import cors from 'cors'
 
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import HomePage from './Components/HomePage/HomePage';
-// import ChefList from './Components/Menu/ChefList';
-// import Logo from './Components/Logo/Logo';
-// import Menu from './Components/MenuPage/Menu';
+import HomePage from './Components/HomePage/HomePage';
+import ChefList from './Components/Menu/ChefList';
+import Logo from './Components/Logo/Logo';
+import Menu from './Components/MenuPage/Menu';
 // import LoginPage from './Components/LoginPage/LoginPage';
-// import ChefForm from './Components/UserChef/ChefForm';
-// import ChefMenu from './Components/UserChef/ChefMenu';
-// import UserForm from './Components/UserForm/UserForm';
-// import ChefProfileFull from './Components/ChefPage/ChefProfileFull';
+import ChefForm from './Components/UserChef/ChefForm';
+import ChefMenu from './Components/UserChef/ChefMenu';
+import UserForm from './Components/UserForm/UserForm';
+import ChefProfileFull from './Components/ChefPage/ChefProfileFull';
 // import LastPage from './Components/LastPageOrdering/LastPage';
-// import Checkout from './Payment/Checkout';
-import { fetchFn } from './Components/api/test';
+import Checkout from './Payment/Checkout';
+// import { fetchFn } from './Components/api/test';
+
 
 
 function App() {
 
   return (
     <div className="App">
-      
-      {/* <Router>
+      <Route path="/chefMenu" element={<ChefMenu/>}/>
+      <Router>
         <Routes>
          
           <Route path="/" element={<Logo/>}/>
           <Route path="/home" element={<HomePage/>} />
           <Route path="/home/:id/ChefList" element={<ChefList />} />
-          <Route path="/home/LoginPage" element={<LoginPage/>}/>
+          {/* <Route path="/home/LoginPage" element={<LoginPage/>}/> */}
           <Route path="/home/chefForm" element={<ChefForm/>}/>
           <Route path="/home/chefForm/chefMenu" element={<ChefMenu/>}/>
           <Route path="/home/chefForm/userForm" element={<UserForm/>}/>
           <Route path="/home/:id/ChefList/:id/chefProfileFull" element={<ChefProfileFull />} />
           <Route path="/:id/chefProfileFull/:id/Menu" element={<Menu/>}/>
           <Route path="/home/user/checkout" element={<Checkout/>}/>
-          <Route path="/home/user/checkout/lastPage" element={<LastPage/>} />
+          {/* <Route path="/home/user/checkout/lastPage" element={<LastPage/>} /> */}
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
-fetchFn()
+// fetchFn()
 export default App;
--- src/Components/api/fetchApi.js
+++ src/Components/api/fetchApi.js
@@ -1,11 +1,24 @@
-// var http = require('http'),
-//     httpProxy = require('http-proxy');
-import http from 'http'
-import httpProxy from 'http'
-const proxy = httpProxy.createProxyServer({});
+// // var http = require('http'),
+// //     httpProxy = require('http-proxy');
+// import http from 'http'
+// import httpProxy from 'http'
+// const proxy = httpProxy.createProxyServer({});
 
-const server = http.createServer(function(req, res) {
-  proxy.web(req, res, { target: 'http://localhost:3001'});
-});
+// const server = http.createServer(function(req, res) {
+//   proxy.web(req, res, { target: 'http://localhost:3001'});
+// });
 
-server.listen(3000);
+// server.listen(3000);
+const fetchMenuFn = async () =>{
+  try {
+    const body = {}
+    const response = fetch('http://localhost:3001/menu',{
+      method: "POST",
+      header:{"Content-Type":"application/json"},
+      body: JSON.stringify(body)
+    })
+    console.log(response)
+  } catch (err) {
+    console.error(err.message)
+  }
+}
